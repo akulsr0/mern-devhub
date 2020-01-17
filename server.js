106fcf1c686dec716bd92be567ec41d8db52b375
akulsrivastava <akulsr0@gmail.com>
2020-01-17 12:49:47 +0530

    added config

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Welcome to DevHub');
